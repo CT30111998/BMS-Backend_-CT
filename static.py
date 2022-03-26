fd249e8b88c6c6ba5717bc28b3aaa7edff0e809f
Kush Mewada <kushmewada18@gmail.com>
2022-03-26 18:26:14 +0530

    add static file for static attribute

---
+++ static.py
@@ -0,0 +1,16 @@
+# static attribute
+
+
+# URLS
+
+# Media urls
+CSS_URL = "/media/css/"
+JS_URL = "/media/js/"
+# End Media urls
+
+# CSS File 
+BOOTSTRAP_CSS = "bs5.css"
+BASE_CSS = "base.css"
+HEADER_CSS = "nbs5.css"
+# End CSS File
+
